a6c084acc8f33d12632f0c067351ba0a3fb6e6b5
steph <stephenwmelnick@gmail.com>
2025-03-23 13:09:35 -0400

    Added colors and other shapes for hitbox preview in anim editor

--- Source/DeckGame/Public/Animation/Notifies/AnimNotifyState_Hitbox.h
+++ Source/DeckGame/Public/Animation/Notifies/AnimNotifyState_Hitbox.h
@@ -18,9 +18,12 @@ class DECKGAME_API UAnimNotifyState_Hitbox : public UAnimNotifyState
 	GENERATED_BODY()
 
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Shape, meta = (AllowPrivateAccess = true))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Shape, Meta = (AllowPrivateAccess))
 	FMeleeHitboxSpawnParameters HitboxParameters;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Shape, Meta = (AllowPrivateAccess))
+	FColor HitboxDisplayColor = FColor::Red;
+	
 	TWeakObjectPtr<UShapeComponent> Hitbox;
 
 
@@ -35,7 +38,14 @@ class DECKGAME_API UAnimNotifyState_Hitbox : public UAnimNotifyState
 
 	void NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference) override;
 	
-	virtual void DrawInEditor(class FPrimitiveDrawInterface* PDI, USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation, const FAnimNotifyEvent& NotifyEvent) const override;
+	virtual void DrawInEditor(FPrimitiveDrawInterface* PDI, USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation, const FAnimNotifyEvent& NotifyEvent) const override;
 	virtual void DrawCanvasInEditor(FCanvas& Canvas, FSceneView& View, USkeletalMeshComponent* MeshComp, const UAnimSequenceBase* Animation, const FAnimNotifyEvent& NotifyEvent) const override;
+
+	/// These functions are identical to those found in draw debug shapes, but working with the PDI instead of a linebatcher
+	static void DrawBox(FPrimitiveDrawInterface* PDI, FVector CurrentPosition, FVector Box, FRotator Rotation, FLinearColor Color);
+	static void DrawSphereHitbox(FPrimitiveDrawInterface* PDI, FVector CurrentPosition, float Radius, FLinearColor Color);
+	static void DrawCapsuleHitbox(FPrimitiveDrawInterface* PDI, FVector CurrentPosition, float Radius, float HalfHeight, FRotator Rotation, FLinearColor Color);
+	static void DrawHalfCircle(FPrimitiveDrawInterface* PDI, const FVector& Base, const FVector& X, const FVector& Y, const FLinearColor& Color, const float Radius, int32 NumSides);
+	static void DrawCircle(FPrimitiveDrawInterface* PDI, const FVector& Base, const FVector& X, const FVector& Y, const FLinearColor& Color, const float Radius, int32 NumSides);
 #endif
 };
